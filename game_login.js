c2dd35abb1706a2942877cf2baa5b6bb22f11f4f
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-04-24 12:11:33 -0300

    Update game_login.js

--- game_login.js
+++ game_login.js
@@ -1,12 +1,11 @@
 
 function addUser() {
-    player1Name = document.getElementById("player1NameInput").value;
-    player2Name = document.getElementById("????").value; //Adicione o ID do segundo player
-  
-      localStorage.setItem("player1Name", player1Name);
-      ?????.setItem("player2Name", player2Name); // Adicione o código localStorage
-  
-      window.location = "???"; //Adicione o nome da segunda página que abrirá quando a função for chamada
-  }
-  
-  
+  player1Name = document.getElementById("player1NameInput").value;
+  player2Name = document.getElementById("player2NameInput").value;
+
+    localStorage.setItem("player1Name", player1Name);
+    localStorage.setItem("player2Name", player2Name);
+
+    window.location = "game_page.html";
+}
+
